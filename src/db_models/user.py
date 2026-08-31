669b4526bc88cc87ced8bfbdccab2411b9fea0fd
Isaac Urban <kpomassiurban@gmail.com>
2026-08-31 11:23:58 +0100

    changed the Column to mapped_column cos of the current version of sqlAlchemy

--- src/db_models/user.py
+++ src/db_models/user.py
@@ -1,5 +1,5 @@
-from sqlalchemy import String, Column, Integer, Boolean, Enum
-from sqlalchemy.orm import Mapped
+from sqlalchemy import String, Integer, Boolean, Enum
+from sqlalchemy.orm import Mapped, mapped_column
 
 from src.database import Base
 from src.models.user_role import UserRole
@@ -8,16 +8,16 @@ from src.models.user_role import UserRole
 class User(Base):
     __tablename__ = "users"
 
-    id: Mapped[int] = Column(Integer, primary_key=True, autoincrement=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
 
-    full_name: Mapped[str] = Column(String(100), nullable=False)
+    full_name: Mapped[str] = mapped_column(String(100), nullable=False)
 
-    email: Mapped[str] = Column(String(100), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
 
-    password: Mapped[str] = Column(String(100), nullable=False)
+    password: Mapped[str] = mapped_column(String(100), nullable=False)
 
-    role: Mapped[UserRole] = Column(Enum(UserRole), default=UserRole.CUSTOMER, nullable=False)
+    role: Mapped[UserRole] = mapped_column(Enum(UserRole), default=UserRole.CUSTOMER, nullable=False)
 
-    address: Mapped[str | None] = Column(String(200), nullable=True)
+    address: Mapped[str | None] = mapped_column(String(200), nullable=True)
 
-    is_active: Mapped[bool] = Column(Boolean, default=False, nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
