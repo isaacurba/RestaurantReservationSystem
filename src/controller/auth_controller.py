9f40a0a52e6687e744940fade4257289807bb931
Isaac Urban <kpomassiurban@gmail.com>
2026-09-01 09:42:08 +0100

    done with auth controller

---
+++ src/controller/auth_controller.py
@@ -0,0 +1,14 @@
+from src.dtos.user import UserCreate, UserLogin, UserResponse
+from src.services.auth_service import AuthService
+
+
+class AuthController:
+
+    def __init__(self, service: AuthService):
+        self.service = service
+
+    def register(self, user_data: UserCreate) -> UserResponse:
+        return self.service.register(user_data)
+
+    def login(self, user_data: UserLogin) -> UserResponse:
+        return self.service.login(user_data)
