42a4243de8d99e66a6e8f857528a148d6656171c
Isaac Urban <kpomassiurban@gmail.com>
2026-09-01 09:45:35 +0100

    fixed import bug in auth service test

--- test/services/test_auth_service_impl.py
+++ test/services/test_auth_service_impl.py
@@ -1,9 +1,9 @@
 import pytest
 
+from src.exceptions import AppException
 from src.database import SessionLocal
 from src.db_models.user import User
-from src.dtos.user import UserCreate
-from src.exceptions.user_already_exist_exception import UserAlreadyExistsException
+from src.dtos.user import UserCreate, UserLogin
 from src.models.user_role import UserRole
 from src.repositories.user_repository_impl import UserRepositoryImpl
 from src.services.auth_service_impl import AuthServiceImpl
@@ -34,8 +34,15 @@ class TestAuthServiceImpl:
             full_name="Isaac Urban",
             email="isaac@mail.com",
             password="password",
+            address="address",
             role=UserRole.CUSTOMER,
             is_active=False,
+        )
+    @pytest.fixture
+    def user_login_data(self):
+        return UserLogin(
+            email="isaac@mail.com",
+            password="password",
         )
 
     def test_register_user(self, service, user_data):
@@ -45,5 +52,21 @@ class TestAuthServiceImpl:
 
     def test_register_user_already_exists(self, service, user_data):
         service.register(user_data)
-        with pytest.raises(UserAlreadyExistsException):
+        with pytest.raises(AppException):
             service.register(user_data)
+
+    def test_register_and_login_user(self, service, user_data, user_login_data):
+        service.register(user_data)
+        result = service.login(user_login_data)
+        assert result.email == "isaac@mail.com"
+        assert result.full_name == "Isaac Urban"
+        assert result.is_active is True
+
+    def test_register_and_login_with_invalid_credentials(self, service, user_data, user_login_data):
+        service.register(user_data)
+        login_data = UserLogin(
+            email="isaac@mail.com",
+            password="wrongpassword",
+        )
+        with pytest.raises(AppException):
+            service.login(login_data)
