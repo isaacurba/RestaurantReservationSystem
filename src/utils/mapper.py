23dd8c6927eba8e995161aff6ed83269b8b6e6aa
Isaac Urban <kpomassiurban@gmail.com>
2026-09-03 13:54:10 +0100

    created mappers for admin service

--- src/utils/mapper.py
+++ src/utils/mapper.py
@@ -1,5 +1,7 @@
-from src.dtos.user import UserCreate
+from src.schemas.user import UserCreate
 from src.db_models.user import User
+from src.db_models.menu_item import MenuItem
+from src.schemas.menu_item import MenuItemCreate, MenuItemUpdate
 
 
 class Mapper:
@@ -13,3 +15,31 @@ class Mapper:
             is_active=user_data.is_active,
         )
         return user
+
+    @staticmethod
+    def map_to_menu_item(item: MenuItemCreate) -> MenuItem:
+        menu_item = MenuItem(
+            name=item.name,
+            price=item.price,
+            category=item.category,
+            description=item.description,
+            menu_id=item.menu_id
+        )
+        return menu_item
+
+    @staticmethod
+    def map_to_update_menu_item(existing_item: MenuItem, item: MenuItemUpdate) -> MenuItem:
+
+        if item.name is not None:
+            existing_item.name = item.name
+
+        if item.price is not None:
+            existing_item.price = item.price
+
+        if item.category is not None:
+            existing_item.category = item.category
+
+        if item.description is not None:
+            existing_item.description = item.description
+
+        return existing_item
